5c8b772e980271f70e83a065783b2104a7c36913
HelloLeeChan <iwar3@qq.com>
2016-08-02 23:37:52 +0800

    test floating

--- DWH5/js/main.js
+++ DWH5/js/main.js
@@ -23,7 +23,7 @@ Page.prototype.start = function(){
         done: function(){
             console.log(that.handle)
             clearInterval(that.handle)
-            contain.style.transform = 'rotateY('+String(Number((/\d+/.exec(contain.style.transform))[0]) +180)+'deg)'
+            //contain.style.transform = 'rotateY('+String(Number((/\d+/.exec(contain.style.transform))[0]) +180)+'deg)'
         } //完成打印后的回调事件
     });
 
@@ -81,7 +81,7 @@ function PageSwap(pages){
         var  container = document.getElementsByClassName('container')[0],
              j = 1
 
-                 container.addEventListener('transitionend',function(e){
+                /* container.addEventListener('transitionend',function(e){
                      if(j < pagesObj.length ){
                          console.log(j)
                          console.log(e)
@@ -96,7 +96,7 @@ function PageSwap(pages){
 
                      }
 
-                 })
+                 })*/
 }
 
 var pages = new PageSwap(document.getElementsByClassName('page'))
